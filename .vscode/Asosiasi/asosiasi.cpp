85c4234946d6361fa91f4b4820ce66a76134d0e4
Krisna <your-email@example.com>
2025-05-27 21:29:11 +0700

    Menambahkan fungsi relasi pasien dan dokter

--- .vscode/Asosiasi/asosiasi.cpp
+++ .vscode/Asosiasi/asosiasi.cpp
@@ -33,3 +33,29 @@ public:
     void tambahPasien(pasien*);
     void cetakPasien();
 };
+
+void pasien::tambahDokter(dokter* pDokter) {
+    daftar_dokter.push_back(pDokter);
+    pDokter->tambahPasien(this);
+}
+
+void dokter::tambahPasien(pasien* pPasien) {
+    daftar_pasien.push_back(pPasien);
+}
+
+void dokter::cetakPasien() {
+    cout << "Daftar Pasien dari dokter \"" << this->nama << "\":\n";
+    for (auto& a : daftar_pasien) {
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
+
+void pasien::cetakDokter() {
+    cout << "Daftar Dokter yang menangani pasien \"" << this->nama << "\":\n";
+    for (auto& a : daftar_dokter) {
+        cout << a->nama << "\n";
+    }
+    cout << endl;
+}
+
